621177efcada1684af63a7a577f2d59d87d0083c
Magone <magoniiites@gmail.com>
2021-06-01 17:11:41 +0200

    Les corrections finales

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,13 +1,13 @@
 const User = require('../models/User'); // Importer modèle de 'User'
 const bcrypt = require('bcrypt');       // Importer le système qui crypte le mot de passe
 const jwt = require('jsonwebtoken');    // Importer le paquet de création de 'Token'
-const MaskData = require('maskdata');   // Importer le paquet pour le masquage d'email
+const maskData = require('maskdata');   // Importer le paquet pour le masquage d'email
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)  // La fonction pour 'hasher' le mot de passe
     .then(hash => {                     // Récuperation de 'hash' de mot de passe
         const user = new User({         // Création de nouveau utilisateur
-            email: MaskData.maskEmail2(req.body.email), // Masquage d'email
+            email: maskData.maskEmail2(req.body.email), // Masquage d'email
             password: hash              // Enregistrement de mot de passe 'haché'
         });
         user.save()                     // Sauvegarde de nouveau utilisateur dans la base de données
@@ -18,7 +18,7 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ email: MaskData.maskEmail2(req.body.email) }) // Pour trouver l'utilisateur correspondant à l'adresse email masqué saisie
+    User.findOne({ email: maskData.maskEmail2(req.body.email) }) // Pour trouver l'utilisateur correspondant à l'adresse email masqué saisie
     .then(user => {
         if (!user) {
             return res.status(401).json({ error: 'Utilisateur non trouvé !' });
@@ -28,12 +28,12 @@ exports.login = (req, res, next) => {
             if (!valid) {
                 return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
-            res.status(200).json({                      // Si l'utilisateur est trouvé
-                userId: user._id,                       // Envoi l'Id d'utilisateur correspondant
-                token: jwt.sign(                        // La fonction de signature de 'token'
-                    { userId: user._id },               // Pour créer l'objet de l'Id de l'utilisateur correspondant
-                    process.env.RANDOM_TOKEN_SECRET,    // Utilisation de clé de 'token' secret, crée avec 'crypto' dans le fichier .env
-                    { expiresIn: '24h' }                // Expiration du 'token' en 24h
+            res.status(200).json({                      // Si l'utilisateur est trouvé             
+                userId: user._id,                       // Envoi l'Id d'utilisateur correspondant             
+                token: jwt.sign(                        // La fonction de signature de 'token'             
+                    { userId: user._id },               // Pour créer l'objet de l'Id de l'utilisateur correspondant             
+                    process.env.RANDOM_TOKEN_SECRET,    // Utilisation de clé de 'token' secret, crée avec 'crypto' dans le fichier .env 
+                    { expiresIn: '24h' }                // Expiration du 'token' en 24h 
                 )
             });
         })
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 ## Piquante
 
 La réalisation du backend d'application des avis gastronomiques - So Pekocko.
+Pour la réalisation du projet le frontend a été cloné depuis : https://github.com/OpenClassrooms-Student-Center/dwj-projet6
 
 ## Frontend
-Pour la réalisation du projet le frontend a été cloné depuis : https://github.com/OpenClassrooms-Student-Center/dwj-projet6
 Les actions faites pour le lancement du frontend:
 - installation npm
 - installation angular/cli@7.0.2
@@ -12,13 +12,14 @@ Les actions faites pour le lancement du frontend:
 
 
 ## Backend
-Le backend du projet a été généré avec Node serveur, framework Express, et la base de données Mongoose
+Le backend du projet a été généré avec Node serveur, framework Express, et la base de données Mongoose.
 Le demarrage du serveur `http://localhost:3000/` avec la commande `nodemon server`
 
 
 ## Securité
 Pour respecter les standards d'OWASP, les mésures de sécurité ont été introduites :
 - Le mot de passe d'utilisateur est chiffré avec package 'bcrypt'
+- L'email d'utilisateur est masqué avec 'maskdata'
 - Les adresses email des utilisateurs sont uniques grâce à package 'mongoose-unique-validator'
 - Deux types d'administrateurs sont définies dans la base de données MongoDB
 - Pour la connexion d'utilisateur le 'token' secret est utilisé qui expire dans 24h. 'jsonwebtoken et 'dotenv' sont installés
